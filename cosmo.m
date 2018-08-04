37431a5dc57c397fa21a686a13729cd9b03b8ea2
eberlot <eva.berlot@gmail.com>
2018-08-04 14:22:37 -0500

    added repetition number, new generated population

--- cosmo.m
+++ cosmo.m
@@ -31,40 +31,50 @@ switch what
         % save the tuning matrix (numNeuron x numStim)
         save(fullfile(dataDir,sprintf('tunMatrix_%dneurons_%dstim',numNeuron,numStim)),'tuning','prefDir');
     case 'GEN_LIF'
-        % define inputs for the LIF
-        % scaling parameters
-        stimRate = 80;
-        gShared  = 3;
-        gIndep   = 2;
-        plotOn   = 0;
-        stimDur  = 2; % in seconds
-        dT = 0.001; % time increment
-        numNeuron = 500;
-        numStim = 10;
-        spikeScale=30;
+        % define default parameters for LIFModel
+        gShared     = 3;
+        gIndep      = 2;
+        plotOn      = 0;
+        stimDur     = 2; % in seconds
+        dT          = 0.001; % time increment
+        numNeuron   = 500;
+        numStim     = 10;
+        numRep      = 50;
+        spikeScale  = 30;
         vararginoptions(varargin,{'stimRate','gShared','gIndep','plotOn','numNeuron','numStim','dt','stimDur'});
         
         TT=[]; % initialise for storage (spikes across neurons / stimuli)
         % load the correct tuning matrix
         D = load(fullfile(dataDir,sprintf('tunMatrix_%dneurons_%dstim',numNeuron,numStim)));
         for t=1:numStim
-            for n=1:numNeuron
-                % 1) determine spike rate based on tuning
-                spkRate = D.tuning(n,t)*spikeScale;
-                % 2) generate spikes
-                [spkInds,spkVec] = genSpikes(stimDur,spkRate,dT);
-                % 3) run the LIFModel
-                T.spikes{1} = LIFModel(spkInds,spkVec,gShared*ones(size(spkVec)),gIndep,dT,plotOn);
-                T.spikeNum  = numel(T.spikes{1});
-                T.neuron = n;
-                T.prefDir = D.prefDir(n);
-                T.stimDir = t;
-                TT = addstruct(TT,T);
+            for r=1:numRep
+                for n=1:numNeuron
+                    % 1) determine spike rate based on tuning
+                    spkRate = D.tuning(n,t)*spikeScale;
+                    % 2) generate spikes
+                    [spkInds,spkVec] = genSpikes(stimDur,spkRate,dT);
+                    % 3) run the LIFModel
+                    T.spikes{1} = LIFModel(spkInds,spkVec,gShared*ones(size(spkVec)),gIndep,dT,plotOn);
+                    T.spikeNum  = numel(T.spikes{1});
+                    T.neuron    = n;
+                    T.prefDir   = D.prefDir(n);
+                    T.stimDir   = t;
+                    TT          = addstruct(TT,T);
+                end
+                fprintf('Generated all neurons for stimulus: %d/%d repetition %d/%d\n',t,numStim,r,numRep);  
             end
         end
-        keyboard;
+        lineplot(TT.stimDir,TT.spikeNum,'split',TT.prefDir,'style_thickline');
+        xlabel('Direction'); ylabel('Spike number'); title('Responses split by preferred direction');
         % save the outputs
         save(fullfile('LIF_%dneurons_%dstim'),'-struct','TT');
+    case 'CHOOSE_subsetPop'
+        % choose subsets of generated population
+        numNeuron = 500;
+        numStim = 10;
+        vararginoptions(varargin,{'numNeuron','numStim'});
+        
+        
     case 'PLOT_LIF'
     otherwise
         fprintf('No such case\n');
