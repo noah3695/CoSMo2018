a1ac0bbfd7ce4e4bb295e342455c2e3dfb9e855c
gnarlytinmen <gnarlytinmen@users.noreply.github.com>
2018-08-03 20:23:56 -0500

    Delete indNoise.m

--- indNoise.m
+++
@@ -1,8 +0,0 @@
-function [gShared] = indNoise(gainNoise,dT,stimDur)
-% Create a vector of voltage noise to serve as independent inputs to cells
-% Usage: [gIndep] = indNoise(gainNoise,dT,stimDur)
-%        [gIndep] = indNoise(3,0.001,1)
-
-gShared = gainNoise*randn(stimDur/dT+1,1);
-
-end
