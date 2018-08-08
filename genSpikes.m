0a465a45ee6c28718115732ba3fcec268fe1cf3d
gnarlytinmen <tyler@ucdavis.edu>
2018-08-08 18:16:59 -0500

    converted to poisson spike trains

--- genSpikes.m
+++ genSpikes.m
@@ -6,16 +6,26 @@ function [spkInds,spkVec] = genSpikes(stimDur,spkRate,dT)
 % Generate a binary spike train over stimulus period
 spkVec = zeros(stimDur/dT + 1,1);
 
+% if spkRate ~= 0 % Allow case for zero stimulus
+%     spkInts = floor(1/(dT*spkRate));
+%     
+%     % Deterministic input spike train
+%     spkInds = 1:spkInts:spkInts*spkRate;
+%     spkVec(spkInds) = 1;
+% else
+%     spkInds = [];
+% end
+
+% Poisson input spike train?
+
 if spkRate ~= 0 % Allow case for zero stimulus
-    spkInts = floor(1/(dT*spkRate));
+
+    spkVec = poissrnd(spkRate*dT,numel(spkVec),1);
+
     
-    % Deterministic input spike train
-    spkInds = 1:spkInts:spkInts*spkRate;
-    spkVec(spkInds) = 1;
+    spkInds = find(spkVec==1);
 else
     spkInds = [];
 end
-
-% Poisson input spike train?
 
 end
