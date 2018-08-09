2d5582266ee5be92a04a8675f1296215156b9c30
eberlot <Eva@eduroam-10-143-7-179.wireless.umn.edu>
2018-08-09 16:22:49 -0500

    changes to mat files

--- cosmo.m
+++ cosmo.m
@@ -79,7 +79,7 @@ switch what
         TC = @(scale,stim,prefDir,sigma,offset)...
                 scale * exp(-((stim-prefDir).^2)./sigma) + offset;     
         popType = 'mixture';
-        vararginoptions(varargin,{'stimRate','gShared','gIndep','plotOn','numNeuron','numStim','dt','stimDur','numRep','spikeScale'});
+        vararginoptions(varargin,{'stimRate','gShared','gIndep','plotOn','numNeuron','numStim','dt','stimDur','numRep','spikeScale','popType'});
         
         TT=[]; % initialise for storage (spikes across neurons / stimuli)
         % load the correct tuning matrix
@@ -88,7 +88,7 @@ switch what
             case 'mixture'
                 anatVec = repmat([1; -1],numNeuron,1);
             case 'positive'
-                anatVec = repmat([1; -1],numNeuron,1);     
+                anatVec = repmat([1; 1],numNeuron,1);     
         end    
         for t=1:numStim
             for r=1:numRun
@@ -138,6 +138,9 @@ switch what
         for l=1:numStim
             legLab{l} = sprintf('stim-%d',l);
         end
+        figure
+        plt.hist(T.spikeNum,'split',T.prefDir);
+        ylabel('number of spikes');
         
         % extract variance and meTan
         T1=tapply(T,{'neuron','prefDir','stimDir'},{'spikeNum','mean','name','spikeNum_mean'},...
@@ -145,14 +148,20 @@ switch what
         % plot responses in dependence of preferred - presented stimulus
         figure(1)
         subplot(1,2,1)
-        lineplot(abs(T1.prefDir-T1.stimDir),T1.spikeNum_mean,'split',T1.prefDir,...
-                'leg',legLab,'style_thickline','markertype','o','markersize',12);
+        %lineplot(abs(T1.prefDir-T1.stimDir),T1.spikeNum_mean,'split',T1.prefDir,...
+        %        'leg',legLab,'style_thickline','markertype','o','markersize',12);
+        plt.line(abs(T1.prefDir-T1.stimDir),T1.spikeNum_mean,'split',T1.prefDir,...
+                'leg',legLab,'markertype','o','markersize',12);
+      
         xlabel('Preferred-presented stimulus');
         ylabel('Mean spike number response');
         subplot(1,2,2)
         % calculate the variance
-        lineplot(abs(T1.prefDir-T1.stimDir),T1.spikeNum_var,'split',T1.prefDir,...
-                'leg',legLab,'style_thickline','markertype','o','markersize',12);
+      %  lineplot(abs(T1.prefDir-T1.stimDir),T1.spikeNum_var,'split',T1.prefDir,...
+       %     'leg',legLab,'style_thickline','markertype','o','markersize',12);
+        plt.line(abs(T1.prefDir-T1.stimDir),T1.spikeNum_var,'split',T1.prefDir,...
+            'leg',legLab,'markertype','o','markersize',12);
+        
         ylabel('Variance in spike number across trials');
         
         % plot stuff
@@ -196,9 +205,13 @@ switch what
                 D.sameNeuron    = double(i==j);
                 pref1           = T1.prefDir(T1.neuron==i);
                 pref2           = T1.prefDir(T1.neuron==j);
+                anat1           = T.anatSign(T.neuron==i);
+                anat2           = T.anatSign(T.neuron==i);
                 D.prefDir1      = pref1(1);
                 D.prefDir2      = pref2(1);
                 D.prefSame      = double(D.prefDir1==D.prefDir2);
+                D.anatSign1     = anat1(1);
+                D.anatSign2     = anat2(1);
                 DD = addstruct(DD,D);
                 Rm(i,j)=D.corrMean;
                 Rv(i,j)=D.corrVar;
@@ -250,7 +263,7 @@ switch what
         figure
         subplot(1,3,1)
         imagesc(M_mean);
-        title('Signal correlation across neuron pairs');
+        title(sprintf('Signal correlation across neuron pairs - %s population',popType));
         subplot(1,3,2)
         imagesc(M_var);
         title('Noise correlation across neuron pairs');
@@ -262,7 +275,7 @@ switch what
         subplot(2,2,1)
         plt.scatter(T.corrMean,T.corrVar,'subset',T.sameNeuron==0 & T.prefSame==0);
         xlabel('Signal correlation'); ylabel('Noise correlation');
-        title('Neurons with diff preferred direction');
+        title(sprintf('Neurons with diff preferred direction - %s population',popType));
         subplot(2,2,2)
         plt.scatter(T.corrMean,T.corrVar,'subset',T.sameNeuron==0 & T.prefSame==1);
         xlabel('Signal correlation'); ylabel('Noise correlation');
